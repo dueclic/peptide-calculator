0c45688ae64879cd3f66f70f448b884eb85f8300
Andrea Debernardi <andrea@debbaweb.it>
2024-07-09 12:46:33 +0200

    small fixes

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -21,14 +21,15 @@ const Calculator: React.FC = () => {
     } = Data;
 
     const [syringeVolume, setSyringeVolume] = useState<number>(defaultSyringeVolume);
-    const [vialQuality, setVialQuality] = useState<number  | 'other'>(defaultVialQuality);
+    const [vialQuality, setVialQuality] = useState<number | 'other'>(defaultVialQuality);
     const [vialQualityOther, setVialQualityOther] = useState<number | null>(null);
-    const [waterVolume, setWaterVolume] = useState<number | 'other' >(defaultWaterVolume);
+    const [waterVolume, setWaterVolume] = useState<number | 'other'>(defaultWaterVolume);
     const [waterVolumeOther, setWaterVolumeOther] = useState<number | null>(null);
     const [dose, setDose] = useState<number | 'other'>(defaultDose);
     const [doseOther, setDoseOther] = useState<number | null>(null);
     const [syringeUnits, setSyringeUnits] = useState<number | null>(null);
     const progressBarEl = useRef<HTMLDivElement>(null);
+    const [submitted, setSubmitted] = useState<boolean>(false);
 
     const vialQualityValue = useMemo(() => {
         if (vialQuality === 'other') {
@@ -53,23 +54,24 @@ const Calculator: React.FC = () => {
 
     const isFormValid = useMemo(() => {
         return !!(vialQualityValue && waterVolumeValue && doseValue);
-    }, [vialQualityValue,waterVolumeValue, doseValue ]);
+    }, [vialQualityValue, waterVolumeValue, doseValue]);
 
     const progressBarWidth = useMemo(() => {
-        if (!syringeUnits || !progressBarEl.current?.offsetWidth){
+        if (!syringeUnits || !progressBarEl.current?.offsetWidth) {
             return 0;
         }
         const progressBarWidth = progressBarEl.current.offsetWidth * syringeUnits / 30;
 
-        if (progressBarWidth > progressBarEl.current.offsetWidth){
+        if (progressBarWidth > progressBarEl.current.offsetWidth) {
             return progressBarEl.current.offsetWidth;
         }
 
         return progressBarWidth;
 
-    }, [syringeUnits,progressBarEl]);
+    }, [syringeUnits, progressBarEl]);
 
     const calculateDose = () => {
+        setSubmitted(true);
         if (vialQualityValue && waterVolumeValue && doseValue) {
             const concentration = vialQualityValue / waterVolumeValue;
             const concentrationMcgMl = concentration * 1000;
@@ -86,23 +88,23 @@ const Calculator: React.FC = () => {
             <div className="section mb-5">
                 <h2 className="text-xl text-alkemya-500 mb-4 font-semibold">{t('syringeVolume')}</h2>
 
-                    <div className="flex justify-center space-x-4">
-                        <Pad onClick={() => setSyringeVolume(0.3)}
-                             selected={syringeVolume === 0.3}
-                             image={`${process.env.PUBLIC_URL}/images/syringe_0.3ml.png`}
-                             text={"0.3ml"}
-                        />
-                        <Pad onClick={() => setSyringeVolume(0.5)}
-                             selected={syringeVolume === 0.5}
-                             image={`${process.env.PUBLIC_URL}/images/syringe_0.5ml.png`}
-                             text={"0.5ml"}
-                        />
-                        <Pad onClick={() => setSyringeVolume(1)}
-                             selected={syringeVolume === 1}
-                             image={`${process.env.PUBLIC_URL}/images/syringe_1ml.png`}
-                             text={"1ml"}
-                        />
-                    </div>
+                <div className="flex justify-center space-x-4">
+                    <Pad onClick={() => setSyringeVolume(0.3)}
+                         selected={syringeVolume === 0.3}
+                         image={`${process.env.PUBLIC_URL}/images/syringe_0.3ml.png`}
+                         text={"0.3ml"}
+                    />
+                    <Pad onClick={() => setSyringeVolume(0.5)}
+                         selected={syringeVolume === 0.5}
+                         image={`${process.env.PUBLIC_URL}/images/syringe_0.5ml.png`}
+                         text={"0.5ml"}
+                    />
+                    <Pad onClick={() => setSyringeVolume(1)}
+                         selected={syringeVolume === 1}
+                         image={`${process.env.PUBLIC_URL}/images/syringe_1ml.png`}
+                         text={"1ml"}
+                    />
+                </div>
             </div>
             <div className="section mb-5">
                 <h2 className="text-xl text-alkemya-500 mb-4 font-semibold">{t('vialQuality')}</h2>
@@ -110,7 +112,7 @@ const Calculator: React.FC = () => {
                     {
                         Object.entries(vialQualities)
                             .map(([_, {text, value}]) => (
-                                    <div  key={value}  className={"flex flex-col justify-between items-center"}>
+                                    <div key={value} className={"flex flex-col justify-between items-center"}>
                                         <Button selected={vialQuality === value}
                                                 onClick={() => setVialQuality(value)} text={t(text)}/>
                                     </div>
@@ -134,7 +136,7 @@ const Calculator: React.FC = () => {
                     {
                         Object.entries(waterVolumes)
                             .map(([_, {text, value}]) => (
-                                    <div key={value}  className={"flex flex-col justify-between items-center"}>
+                                    <div key={value} className={"flex flex-col justify-between items-center"}>
                                         <Button selected={waterVolume === value}
                                                 onClick={() => setWaterVolume(value)} text={t(text)}/>
                                     </div>
@@ -146,7 +148,7 @@ const Calculator: React.FC = () => {
                 <div className="flex justify-center space-x-1 md:space-x-4">
                     <div className={"flex flex-col justify-between items-center"}>
                         {waterVolume === 'other' &&
-                            <InputNumber value={vialQualityOther}  onChange={(ev) => setWaterVolumeOther(
+                            <InputNumber value={vialQualityOther} onChange={(ev) => setWaterVolumeOther(
                                 parseFloat(ev.target.value)
                             )} placeholder={t('enterValueInMl')}/>}
                     </div>
@@ -158,7 +160,7 @@ const Calculator: React.FC = () => {
                     {
                         Object.entries(doses)
                             .map(([_, {text, value}]) => (
-                                    <div key={value}  className={"flex flex-col justify-between items-center"}>
+                                    <div key={value} className={"flex flex-col justify-between items-center"}>
                                         <Button selected={dose === value}
                                                 onClick={() => setDose(value)} text={t(text)}/>
                                     </div>
@@ -181,16 +183,22 @@ const Calculator: React.FC = () => {
                         className="calculate-btn font-colony  mt-5 p-2 text-xl uppercase text-black border border-black font-semibold bg-alkemya-500 disabled:bg-yellow-200"
                         onClick={calculateDose}>{t('calculate')}</button>
             </div>
-            <div className="result mt-5 text-lg text-alkemya-500 font-semibold" id="result">
-                {!syringeUnits && t('invalid_values')}
-                {syringeUnits && syringeUnits && t('dose_instructions', { dose: doseValue, units: syringeUnits.toFixed(2) })}
-            </div>
 
-            <div  ref={progressBarEl}  className={"bg-no-repeat bg-contain border-0 w-full h-[60px] bg-white"} style={{
-                backgroundImage: `url('${process.env.PUBLIC_URL}/images/30ml-04.png')`
-            }}>
-                <div id="my-progress" className={"bar border-0 bg-yellow-100 bg-opacity-50 h-full"} style={{
-                    width: progressBarWidth+'px'
+            {submitted && <div className="result mt-5 text-lg text-alkemya-500 font-semibold" id="result">
+                    {!syringeUnits && t('invalid_values')}
+                    {syringeUnits && syringeUnits && t('dose_instructions', {
+                        dose: doseValue,
+                        units: syringeUnits.toFixed(2)
+                    })}
+                </div>}
+
+
+            <div ref={progressBarEl} className={`bg-no-repeat bg-contain border-0 w-full h-[60px] bg-white ${!submitted && 'invisible'}`}
+                 style={{
+                     backgroundImage: `url('${process.env.PUBLIC_URL}/images/30ml-04.png')`
+                 }}>
+                <div id="my-progress" className={"bar border-0 bg-alkemya-500 bg-opacity-50 h-full"} style={{
+                    width: progressBarWidth + 'px'
                 }}></div>
             </div>
 
