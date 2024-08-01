e16c60a0378c49931c4873b912a140770dfa2431
Andrea Debernardi <andrea@debbaweb.it>
2024-08-01 18:05:39 +0200

    change root

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,22 @@ import reportWebVitals from './reportWebVitals';
 
 import './i18n';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const target =  document.getElementById('alkw-root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+if (target){
+
+    const root = ReactDOM.createRoot(
+      target
+    );
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+
+    // If you want to start measuring performance in your app, pass a function
+    // to log results (for example: reportWebVitals(console.log))
+    // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+    reportWebVitals();
+
+}
